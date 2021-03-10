9211f42ba3762d8134d79b4c444c7fcdaea0cdeb
sonnguyen2djv <son.nguyen2@drjoy.vn>
2021-03-10 19:25:14 +0900

    3

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const HANNOM_WEB_ROOT_URL = 'http://hvdic.thivien.net/whv/';
 // const WORD_TO_FIND = '検';
 const WORD_TO_FIND = '%E6%A4%9C';
 axios.get(HANNOM_WEB_ROOT_URL + WORD_TO_FIND).then(result=>{
-  console.log(result.data);
+  const html = result.data;
+  const $ = cheerio.load(html);
+  const text = $('.hvres-meaning > a').first().text();
+  console.log(text);
 }, rejectReason=>{
   console.error(rejectReason);
 });
